bc14cd81bfcb26354560dbc1eb4f66f0f275c339
abbccdda <boyang@confluent.io>
2019-12-13 11:37:56 -0800

    consumer app writeup

--- run_local.sh
+++ run_local.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 PROJECT_NAME=lekafgo-hackday
+METRIC_FILE_DIR=/tmp/consumer-driver/
+
 ./gradlew distTar
 cp build/distributions/${PROJECT_NAME}.tar.gz local_build/
 cd local_build
@@ -8,6 +10,8 @@ tar zxvf ${PROJECT_NAME}.tar.gz
 cd ${PROJECT_NAME}
 tar zxvf ${PROJECT_NAME}-libs.tar.gz
 cd ../
-pwd
+
+mkdir -p ${METRIC_FILE_DIR}
+touch ${METRIC_FILE_DIR}/$1-thread-consumer.csv
 java -cp "./${PROJECT_NAME}/${PROJECT_NAME}-libs/*" -Dlog4j.configuration=file:./${PROJECT_NAME}/config/log4j.properties \
-                 demo.driver.ConsumerAppDriver ./${PROJECT_NAME}/config/single-thread-consumer.properties
+                 demo.driver.ConsumerAppDriver ./${PROJECT_NAME}/config/$1-thread-consumer.properties
