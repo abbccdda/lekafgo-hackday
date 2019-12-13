81c9fdccb7ecb8072c99ef713f2dc23cb56b1144
abbccdda <boyang@confluent.io>
2019-12-13 15:14:19 -0800

    add run script

---
+++ run_local.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME=lekafgo-hackday
+./gradlew distTar
+cp build/distributions/${PROJECT_NAME}.tar.gz local_build/
+cd local_build
+tar zxvf ${PROJECT_NAME}.tar.gz
+cd ${PROJECT_NAME}
+tar zxvf ${PROJECT_NAME}-libs.tar.gz
+cd ../
+java -cp "./${PROJECT_NAME}/${PROJECT_NAME}-libs/*" -Dlog4j.configuration=file:./${PROJECT_NAME}/config/log4j.properties \
+                 demo.driver.ConsumerAppDriver ./${PROJECT_NAME}/config/consumer.properties
